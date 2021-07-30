91473ccafd4276cc9cbaf359da1dc114cdc3010d
RemusCourtenay <remuscourtenay@gmail.com>
2021-07-31 11:51:03 +1200

    Added extra stuff to commit

---
+++ frontend/src/components/EmailTab/EmailTab.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './EmailTab.css';
+
+const EmailTab = () => (
+  <div className="EmailTab">
+    EmailTab Component
+  </div>
+);
+
+EmailTab.propTypes = {};
+
+EmailTab.defaultProps = {};
+
+export default EmailTab;
