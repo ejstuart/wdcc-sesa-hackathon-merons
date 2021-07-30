b1edfd5a95c2bdf72d3d8133da813b2f7b8872b6
ejstuart <estu836@aucklanduni.ac.nz>
2021-07-30 23:47:07 +1200

    Setup frontend folder

---
+++ frontend/src/components/Box/Box.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Box.module.css';
+
+const Box = () => (
+  <div className={styles.Box}>
+    BMAIL
+  </div>
+);
+
+Box.propTypes = {};
+
+Box.defaultProps = {};
+
+export default Box;
