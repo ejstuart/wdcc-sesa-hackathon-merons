8c5a67b3611b45659cf6356e3ed8e0c40b9efcbd
ejstuart <estu836@aucklanduni.ac.nz>
2021-07-31 02:35:48 +1200

    Some EmailTabContainer Functionality implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import React from 'react';
 import Box from "./components/Box/Box";
 import ScoreSidebar from "./components/ScoreSidebar/ScoreSidebar";
+import EmailTabContainer from "./components/EmailTabContainer/EmailTabContainer";
 
 function App() {
   return (
     <div className="App">
       <ScoreSidebar/>
+        <EmailTabContainer/>
     </div>
   );
 }
