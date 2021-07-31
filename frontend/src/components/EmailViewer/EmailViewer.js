a31c67b470139a394086258afb49d275f4aa830c
ejstuart <estu836@aucklanduni.ac.nz>
2021-07-31 22:33:14 +1200

    Added scrollbar to email text

--- frontend/src/components/EmailViewer/EmailViewer.js
+++ frontend/src/components/EmailViewer/EmailViewer.js
@@ -7,40 +7,42 @@ import TextField from '@material-ui/core/TextField';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 
 const EmailViewer = (props) => (
-  <div className={styles.EmailViewer}>
+    <div className={styles.EmailViewer}>
 
-      <div className={styles.sender_head}>
-          <div className={styles.circle}></div>
+        <div className={styles.sender_head}>
+            <div className={styles.circle}></div>
 
-          <div className={styles.sender_info}>
-              <Typography className={styles.email_text} variant={"h5"}>John Name</Typography>
-              <Typography className={styles.email_text}>Name@jasnkda.com</Typography>
-          </div>
-      </div>
-      <div className={styles.email_body}>
-          <Typography className={styles.email_text} paragraph>Want to MAKE CASH FAST? JUST CLICK THE FOLLOWING LINK:
-                <p></p>
-              <u>HERE CLICK ME</u>
-              <p></p>
-              I made over $10000000000 in just ONE DAY!
-          </Typography >
+            <div className={styles.sender_info}>
+                <Typography className={styles.email_text} variant={"h5"}>John Name</Typography>
+                <Typography className={styles.email_text}>Name@jasnkda.com</Typography>
+            </div>
+        </div>
+        <div className={styles.email_body}>
+            <div className={styles.email_body_text}>
+                <Typography className={styles.email_text} paragraph>Want to MAKE CASH FAST? JUST CLICK THE FOLLOWING LINK:
+                    <p></p>
+                    <u>HERE CLICK ME</u>
+                    <p></p>
+                    I made over $10000000000 in just ONE DAY!
+                </Typography >
+            </div>
 
-          <TextField variant={"outlined"} fullWidth multiline rows={3} disabled>
+            <TextField variant={"outlined"} fullWidth multiline rows={3} disabled>
 
-          </TextField>
-          <div>
-              <ButtonGroup variant="text" color="primary" style={{float: 'left'}}>
-                  <Button><b>Bold</b></Button>
-                  <Button><i>Italic</i></Button>
-                  <Button><u>Underline</u></Button>
-              </ButtonGroup>
-              <Button style={{float: 'right'}} onClick={props.replyFunction}>Reply</Button>
-          </div>
+            </TextField>
+            <div>
+                <ButtonGroup variant="text" color="primary" style={{float: 'left'}}>
+                    <Button><b>Bold</b></Button>
+                    <Button><i>Italic</i></Button>
+                    <Button><u>Underline</u></Button>
+                </ButtonGroup>
+                <Button style={{float: 'right'}} onClick={props.replyFunction}>Reply</Button>
+            </div>
 
-      </div>
+        </div>
 
 
-  </div>
+    </div>
 );
 
 EmailViewer.propTypes = {
