8bf1d23d41de9506f45eabe181e634722ef67881
ijoh785 <ijoh785@aucklanduni.ac.nz>
2021-07-31 12:59:01 +1200

    add: sidebar

--- frontend/src/components/ScoreSidebar/ScoreSidebar.js
+++ frontend/src/components/ScoreSidebar/ScoreSidebar.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './ScoreSidebar.module.css';
+import {Drawer, List, ListItem, ListItemIcon, ListItemText} from "@material-ui/core";
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+const menuList = () => (
+    <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                <ListItemText primary={text} />
+            </ListItem>
+        ))}
+    </List>
+);
 
 const ScoreSidebar = () => (
   <div className={styles.ScoreSidebar}>
-    ScoreSidebar Component
+      <Drawer anchor={"left"} open={true} variant={"permanent"}>
+          {menuList()}
+      </Drawer>
   </div>
 );
 
